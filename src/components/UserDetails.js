af7b8288ae742d4d9773b22161fb15fe74103cd4
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-24 11:50:32 +0100

    URL Params

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
 
 const UserDetails = () => {
+    const {userId}=useParams()
+    
   return <div>
-      user Details
+      user Details {userId}
   </div>;
 };
 
