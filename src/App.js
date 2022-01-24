7a08c527cc28ac9ac280d18e44680a8dc22321e4
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-24 11:46:03 +0100

    dynamic route

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-
-import  {Route,Routes} from 'react-router-dom'
-import  {Home} from './components/Home'
-import  {About} from './components/About'
-import Navbar from './components/Navbar';
-import OrderConfirm from './components/OrderConfirm';
-import NoMatch from './components/NoMatch';
-import Products from './components/Products';
-import FeatureProducts from './components/FeatureProducts';
-import NewProducts from './components/NewProducts';
-
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import Navbar from "./components/Navbar";
+import OrderConfirm from "./components/OrderConfirm";
+import NoMatch from "./components/NoMatch";
+import Products from "./components/Products";
+import FeatureProducts from "./components/FeatureProducts";
+import NewProducts from "./components/NewProducts";
+import UserDetails from "./components/UserDetails";
+import Users from "./components/Users";
+import Admin from "./components/Admin";
 
 function App() {
   return (
     <>
-     <Navbar/>
-     <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='about' element={<About />}></Route>
-      <Route path='order-cofirmed' element={<OrderConfirm/>}></Route>
-      <Route path='/products' element={<Products/>}>
-         <Route index element={<FeatureProducts />} />
-         <Route path='feature' element={<FeatureProducts />} />
-         <Route path='new' element={<NewProducts />} />
-      </Route>
-      <Route path='*' element={<NoMatch/>}></Route>
-     </Routes>    
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="order-cofirmed" element={<OrderConfirm />}></Route>
+        <Route path="/products" element={<Products />}>
+          <Route index element={<FeatureProducts />} />
+          <Route path="feature" element={<FeatureProducts />} />
+          <Route path="new" element={<NewProducts />} />
+        </Route>
+        <Route path="*" element={<NoMatch />}></Route>
+        <Route path="/users" element={<Users />}>
+          <Route path=":userId" element={<UserDetails />} />
+          <Route path="admin" element={<Admin />} />
+        </Route>
+      </Routes>
     </>
-
-  )
+  );
 }
 
 export default App;
