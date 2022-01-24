99b18b8e6d36599333f53345f3a19c6d2c1e3cd9
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-24 12:22:16 +0100

    Search Params

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const UserDetails = () => {
-    const {userId}=useParams()
-    
-  return <div>
-      user Details {userId}
-  </div>;
+  const { userId } = useParams();
+
+  return <div>user Details {userId}</div>;
 };
 
 export default UserDetails;
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet, useSearchParams } from "react-router-dom";
 
 const Users = () => {
-  return <div>
-        Users page
-        <Outlet/>
-  </div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const isActive = searchParams.get("filter") == "active";
+
+  return (
+    <div>
+      Users page
+      <Outlet />
+      <button onClick={() => setSearchParams({ filter: "active" })}>
+        active
+      </button>
+      <button onClick={() => setSearchParams({})}>desactive</button>
+
+      {/* {isActive &&<h1>active section</h1> }
+      {!isActive &&<h1>NO ACTIVE section</h1>}  */}
+      {isActive ? <h1>active section</h1> : <h1>NO ACTIVE section</h1>}
+
+    </div>
+  );
 };
 
 export default Users;
